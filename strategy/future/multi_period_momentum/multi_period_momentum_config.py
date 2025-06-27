0a2b94e90c742b7e519824a9f210e585a061502f
hyunho.lee <hyunho.lee@zeroone.ai>
2025-06-27 14:04:19 +0900

    fix:moidfy test strategy config

--- strategy/future/multi_period_momentum/multi_period_momentum_config.py
+++ strategy/future/multi_period_momentum/multi_period_momentum_config.py
@@ -1,26 +1,19 @@
 # ==========================
 # Required System Settings
 # ==========================
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.abspath(os.path.join(__file__, "../../../../module/.env"))
+load_dotenv(dotenv_path)
+DATA_KEY = os.getenv("DATA_KEY")
 
 system_config = {
-    "data_apikey": "Input User Data Api Key", # CoinAPI - data api key
+    "data_apikey": DATA_KEY, # CoinAPI - data api key
     "strategy_name": "multi_period_momentum", # User strategy file name
-    "trading_hours": 72, # System run time
+    "trading_hours": 6, # System run time
     "base_symbol": "BTCUSDT",
-    "symbols": ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'BCHUSDT', 'LTCUSDT', 
-                'ADAUSDT', 'ETCUSDT', 'TRXUSDT', 'DOTUSDT', 'DOGEUSDT', 
-                'SOLUSDT', 'BNBUSDT', 'ICPUSDT', 'FILUSDT', 'XLMUSDT',
-                'ONTUSDT', 'QTUMUSDT', 'NKNUSDT', 'AVAXUSDT', 'CELOUSDT',
-                'WAXPUSDT', 'DYMUSDT', 'APTUSDT', 'FLOWUSDT', 'GTCUSDT',
-                'SEIUSDT', 'ATOMUSDT', 'NEARUSDT', 'STXUSDT', 'MINAUSDT',
-                'BSVUSDT', 'EGLDUSDT', 'RVNUSDT', 'ONEUSDT', 'NEOUSDT',
-                'JUPUSDT', 'ZILUSDT', 'XTZUSDT', 'LUNCUSDT', 'CKBUSDT',
-                'IOTAUSDT', 'THETAUSDT', 'ICXUSDT', 'ALGOUSDT', 'LSKUSDT', 
-                'CFXUSDT', 'TONUSDT', 'MEMEUSDT', 'SXPUSDT', 'KASUSDT',
-                'HBARUSDT', 'IOSTUSDT', 'BEAMUSDT', 'FETUSDT', 'XVGUSDT', 
-                'SUIUSDT', 'VETUSDT', 'KSMUSDT', 'ARBUSDT', 'ARUSDT', 
-                'RUNEUSDT', 'IOTXUSDT', 'TAIKOUSDT', 'COREUSDT', 'BBUSDT', 
-                'COTIUSDT', 'NTRNUSDT'], # List of all currently available symbols: define only the symbols you need as values.
+    "symbols": ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'BCHUSDT', 'LTCUSDT', 'ADAUSDT', 'ETCUSDT', 'TRXUSDT', 'DOTUSDT', 'DOGEUSDT'], # List of symbols to use
     "productType": "usdt-futures",
     "posMode": "hedge_mode", # one_way_mode , hedge_mode
     "marginMode": "crossed", # isolated
@@ -28,17 +21,24 @@ system_config = {
     "marginCoin": "usdt",
     "orderType": "market",
     "timeframe": "1min",
-    "tradeType": "future", # future
+    "tradeType": "future",
     "is_portfolio": True,   
-    "total_allocation": 1.0, # Proportion of total assets to use
+    "total_allocation": 0.95, # Proportion of total assets to use
     "leverage": 10, # Leverage
-    "new_data_window": 60, # The window value for fetching the latest data (preferably the maximum value of the strategy parameter)
-    "weight_method": "custom", # equal, split(long/short), custom
-    "custom_weights": {    # Required if weight_method is custom
-        "BTCUSDT" : "0.5",
-        "ETHUSDT" : "0.3",
-        "XRPUSDT" : "0.2"
-    }    
+    "new_data_window": 72, # The window value for fetching the latest data (preferably the maximum value of the strategy parameter)
+    "weight_method": "custom", # 가중치 메소드 - equal, split(long/short), custom
+    "custom_weights": { # 예시
+        'BTCUSDT': 0.5,
+        'ETHUSDT': 0.2,
+        'XRPUSDT': 0.1,
+        'BCHUSDT': 0.04,
+        'LTCUSDT': 0.04,
+        'ADAUSDT': 0.03,
+        'ETCUSDT': 0.03,
+        'TRXUSDT': 0.03,
+        'DOTUSDT': 0.02,
+        'DOGEUSDT': 0.01
+    }
 }
 
 # ==========================
@@ -55,9 +55,9 @@ rebalancing_config = {
 # Strategy Parameter Settings
 # ==========================
 
-hours = [1,3,6]
+hours = [1,2,3]
 strategy_config = {
-    "long_maximum_candidates": 2,
-    "short_maximum_candidates": 1,
+    "long_maximum_candidates": 5,
+    "short_maximum_candidates": 5,
     "minutes": [int(i*60) for i in hours]
 }
