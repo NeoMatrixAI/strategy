a8a7ab43a97e5f937cbede7302110a4044f78b1c
NeoMatrix <neo.matrix.zeroone@gmail.com>
2025-08-20 08:15:03 +0900

    Update multi_period_momentum_config.py

--- futures/multi_period_momentum/multi_period_momentum_config.py
+++ futures/multi_period_momentum/multi_period_momentum_config.py
@@ -1,46 +1,4 @@
 # ==========================
-# Required System Settings
-# ==========================
-import os
-
-system_config = {
-    "data_apikey": "a71eaf04-802f-40be-93c2-5bee2548f4db",
-    "strategy_name": "multi_period_momentum", # User strategy file name
-    "trading_hours": 1, # System run time
-    "base_symbol": "BTCUSDT",
-    "symbols": ['BTCUSDT', 'ETHUSDT'], # List of symbols to use
-    "productType": "usdt-futures",
-    "posMode": "hedge_mode", # one_way_mode , hedge_mode
-    "marginMode": "crossed", # isolated
-    "holdSide": "long", # long , short
-    "marginCoin": "usdt",
-    "orderType": "market",
-    "timeframe": "1min",
-    "tradeType": "futures",
-    "is_portfolio": True,   
-    "total_allocation": 0.95, # Proportion of total assets to use
-    "leverage": 3, # Leverage
-    "new_data_window": 72, # The window value for fetching the latest data (preferably the maximum value of the strategy parameter)
-    "weight_method": "custom", # Weight method(equal, split, custom)  "equal" : Full equal split
-                                                                    # "split" : After splitting spot/futures in half(spot:0.5 , futures:0.5), equal ratio
-                                                                    # "custom" : User custom ratio
-    "custom_weights": { # 예시
-        'BTCUSDT': 0.5,
-        'ETHUSDT': 0.5
-    }
-}
-
-# ==========================
-# Rebalancing Trade Parameters
-# ==========================
-
-rebalancing_config = {
-    "rebalancing_interval_hours": 1/12, # Rebalancing cycle (hours)
-    "minimum_candidates": 0
-}
-
-
-# ==========================
 # Strategy Parameter Settings
 # ==========================
 
