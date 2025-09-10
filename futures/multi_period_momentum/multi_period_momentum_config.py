e2b30f4e21e2af8c03d56b9839653518d1b0a0f3
NeoMatrix <neo.matrix.zeroone@gmail.com>
2025-09-10 14:11:01 +0900

    Update multi_period_momentum_config.py

--- futures/multi_period_momentum/multi_period_momentum_config.py
+++ futures/multi_period_momentum/multi_period_momentum_config.py
@@ -4,9 +4,5 @@
 
 hours = [1,2,3]
 strategy_config = {
-    "long_maximum_candidates": 2,
-    "long_minimum_candidates": 0,
-    "short_maximum_candidates": 1,
-    "short_minimum_candidates": 0,
     "minutes": [int(i*60) for i in hours]
 }
