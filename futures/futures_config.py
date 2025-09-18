29e870540fc6813ee0d90da32fac1abb135a729a
NeoMatrix <neo.matrix.zeroone@gmail.com>
2025-09-18 14:35:39 +0900

    rollback to v01.00.00

--- futures/futures_config.py
+++ futures/futures_config.py
@@ -2,7 +2,9 @@
 # Required System Settings
 # ==========================
 system_config = {
+    "data_apikey": "a71eaf04-802f-40be-93c2-5bee2548f4db",
     "tz_str": "Asia/Seoul", 
+    "timeframe": "1min",
     "orderType": "market",
     "base_symbol": "BTCUSDT",
     "productType": "usdt-futures",
@@ -10,6 +12,7 @@ system_config = {
     "holdSide": "long",                   
     "marginMode": "crossed",
     "marginCoin": "usdt",
+    "symbols": ['BTCUSDT', 'ETHUSDT', 'LTCUSDT', 'TRXUSDT', 'DOTUSDT', 'DOGEUSDT', 'XRPUSDT', 'BCHUSDT', 'ADAUSDT'],
     "leverage": 5,
     "trading_hours": 6,
     "total_allocation": 0.4, 
